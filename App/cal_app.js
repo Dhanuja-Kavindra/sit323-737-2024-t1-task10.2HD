4a501b1a97fc8f173af3e5c88a0bfa858323a5ea
Dhanuja Kavindra <spdanuja@gmail.com>
2024-05-25 19:01:26 +1000

    Final Project-missing files2

--- App/cal_app.js
+++ App/cal_app.js
@@ -2,7 +2,7 @@ function calculate(operation) {
     const num1 = document.getElementById('num_1').value;
     const num2 = document.getElementById('num_2').value;
     
-    const url = `http://localhost:8080/${operation}?num1=${num1}&num2=${num2}`;
+    const url = `http://35.238.157.1/${operation}?num1=${num1}&num2=${num2}`;
 
     fetch(url, {
         credentials: 'include' // Ensures cookies are sent with the request
